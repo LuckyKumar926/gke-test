48e89a0c843ea74ccf73bfc8110e3fcbbc877b39
LuckyKumar926 <kumarlaxmangc@gmail.com>
2024-07-17 23:11:47 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,22 +1,13 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+from flask import Flask
 
+# Create a Flask application
+app = Flask(__name__)
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+# Define a route for the root URL
+@app.route('/')
+def hello_world():
+    return 'Hello, World App 1!'
 
-
+# Run the Flask application if this file is executed directly
 if __name__ == '__main__':
-    main()
+    app.run(debug=True, port=8080,host='0.0.0.0')
