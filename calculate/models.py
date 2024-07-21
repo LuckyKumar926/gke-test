d3a39440c49803f4fa476795b3b4e79cb66f256f
LuckyKumar926 <kumarlaxmangc@gmail.com>
2024-07-21 16:56:15 -0400

    Delete calculate directory

--- calculate/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Calculate(models.Model):
-    file = models.CharField(max_length=500),
-    product = models.CharField(max_length=500)
--- calculate/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from calculate.views import CalculateView
-
-urlpatterns = [
-    path('calculate', CalculateView.as_view()),
-]
